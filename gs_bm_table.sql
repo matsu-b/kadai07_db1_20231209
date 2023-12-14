57ac96add2f5c519fa8c7c9ed4f3e43565646456
matsu-b <110808746+matsu-b@users.noreply.github.com>
2023-12-15 00:31:12 +0900

    Add files via upload
    SQL

---
+++ gs_bm_table.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: localhost
+-- 生成日時: 2023 年 12 月 14 日 16:27
+-- サーバのバージョン： 10.4.28-MariaDB
+-- PHP のバージョン: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `gs_kadai`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_bm_table`
+--
+
+CREATE TABLE `gs_bm_table` (
+  `id` int(12) NOT NULL,
+  `book` varchar(64) NOT NULL,
+  `url` text NOT NULL,
+  `comment` text NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `gs_bm_table`
+--
+
+INSERT INTO `gs_bm_table` (`id`, `book`, `url`, `comment`, `date`) VALUES
+(1, '目的ドリブンの思考法', 'https://amzn.asia/d/fcaGmx1', 'てすと登録', '2023-12-14 23:30:38'),
+(2, '未来に先回りする思考法', 'https://amzn.asia/d/8SPLyxR', '読みかけの本', '2023-12-14 23:53:39'),
+(3, '多様性の科学', 'https://amzn.asia/d/eZiE8XZ', '読んでみたい', '2023-12-15 00:07:57');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `gs_bm_table`
+--
+ALTER TABLE `gs_bm_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
